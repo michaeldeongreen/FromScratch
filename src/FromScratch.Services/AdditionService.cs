f66ad6422c1b14dfc3185f893d7917b102f780e0
michaeldeongreen <michaeldeongreen@gmail.com>
2017-09-09 13:05:52 -0500

    *Added unit test project *Added service project *Created addition service to start seeing if I can mock unit of work

---
+++ src/FromScratch.Services/AdditionService.cs
@@ -0,0 +1,24 @@
+﻿using FromScratch.Domain.Interfaces;
+using FromScratch.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FromScratch.Services
+{
+    public class AdditionService : IAdditionService
+    {
+        private readonly IUnitOfWork _uow;
+        public AdditionService(IUnitOfWork uow)
+        {
+            _uow = uow;
+        }
+        public int Add(int number1, int number2)
+        {
+            int value = number1 + number2;
+            return value;
+        }
+    }
+}
---
+++ src/FromScratch.Unit.Tests/AdditionServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FromScratch.Unit.Tests
+{
+    [TestFixture]
+    public class AdditionServiceTests
+    {
+        [Test]
+        public void Can_Add_Two_Numbers_Test()
+        {
+            //given
+            int num1 = 1, num2 = 4;
+            //when
+            //then
+        }
+    }
+}
